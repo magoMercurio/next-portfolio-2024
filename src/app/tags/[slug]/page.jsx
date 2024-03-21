cf0e5a5e2fd96e30366d631d6349fcc8b6de4efc
mago_d_Mercurio <davila.feria@gmail.com>
2024-03-20 23:09:03 -0500

    create tags page

---
+++ src/app/tags/[slug]/page.jsx
@@ -0,0 +1,10 @@
+
+
+
+export default function Tagpage() {
+    return (
+        <div>
+            <h1>slug de tags</h1>
+        </div>
+    )
+}
---
+++ src/app/tags/page.jsx
@@ -0,0 +1,9 @@
+
+
+export default function TagsPage() {
+  return (
+    <div>
+      <h1>Tags Page</h1>
+    </div>
+  ) 
+}

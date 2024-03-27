6e376c8b8ee60738a84c55c26d7c819da56b1d2d
mago_d_Mercurio <davila.feria@gmail.com>
2024-03-26 21:30:15 -0500

    Update proyectdata.js

--- src/data/proyectdata.js
+++ src/data/proyectdata.js
@@ -6,6 +6,7 @@ export const proyectsdata = [
     "descripcion": "landing page creada usando Nexjs y Tailwindcss",
     "link": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     "link_github": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: [frontend, landingpage,nextjs, tailwindcss],
   },
   {
     "titulo": "Raul Davila Psicologo V2",
@@ -14,24 +15,34 @@ export const proyectsdata = [
     "descripcion": "landing page creada usando astro js y tailwindcss",
     "link": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     "link_github": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-
+    tags: [frontend, landingpage,nextjs, tailwindcss],
   },
   {
     "titulo": "Raul Davila Psicologo v1",
     "slug": "landingpage-web-blog-psicologo-v1",
     "imagen": "/images/rpsicologo-1f8ec2f2.webp",
     "descripcion": "landing page creada usando reactjs",
+    "link": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "link_github": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: [frontend, landingpage,nextjs, tailwindcss],
   },
   {
     "titulo": "Maribel Tristan Psicologa",
     "slug": "landingpage-web-psicologa",
     "imagen": "/images/mtp-landp-b460dcc3.webp",
     "descripcion": "landing page creada usando astroJs y Tailwindcss",
+    "link": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "link_github": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: [frontend, landingpage,nextjs, tailwindcss],
   },
   {
     "titulo": "Barberia Vintage",
     "slug": "landingpage-web-barberia-vintage",
     "imagen": "/images/barberiaimg-570f5463.webp",
     "descripcion": "landing page creada usando html y css",
+    "link": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "link_github": "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    tags: [frontend, landingpage,nextjs, tailwindcss],
+
   }
 ];

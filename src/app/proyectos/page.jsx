1c0e1b46f0429a408231a39e9d5a8a703e651f43
mago_d_Mercurio <davila.feria@gmail.com>
2024-04-04 19:15:48 -0500

    Update page.jsx

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -3,14 +3,14 @@ import { Card } from "@/components/Card";
 import { proyectsdata } from "@/data/proyectdata";
 
 export const metadata = {
-  title: "raul davila - proyectos",
-  description: "porfolio de proyectos web que he desarrollado",
+  title: "portfolio de proyectos",
+  description: "Explora una variedad de proyectos web que reflejan mi experiencia y creatividad en el desarrollo digital.",
 };
 
 export default function ProyectsPage() {
   return (
     <main className="section mb-8">
-      <h1>{metadata.title}</h1>
+      <h1 className="mb-4">{metadata.title}</h1>
       <p>{metadata.description}</p>
       <article className=" mt-8 flex flex-col">
       {proyectsdata.map((proyect) => {

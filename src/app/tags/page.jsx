14a488c47a37362706aca1f34e9af7f01b9f18ca
mago_d_Mercurio <davila.feria@gmail.com>
2024-04-08 16:02:40 -0500

    Update page.jsx

--- src/app/tags/page.jsx
+++ src/app/tags/page.jsx
@@ -1,9 +1,15 @@
+import { proyectsdata } from "@/data/proyectdata";
+
+
 
 
 export default function TagsPage() {
   return (
     <main>
       <h1>Tags Page</h1>
+      <article>
+
+      </article>
     </main>
   ) 
 }
